c6ed5cd3c11de5bce5a66a04b69d18c787bc7740
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2021-11-30 17:29:48 -0500

    Something stuff more

--- main2.py
+++ main2.py
@@ -1,24 +1,43 @@
-import pandas as pd
 import pprint
+
+import pandas as pd
+import argparse
 import tweepy
 import tweepy as tp
 
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", default="output.csv")
+args = parser.parse_args()
+
 
 def authorize():
-def add(df, data):
+    auth = tp.OAuthHandler(os.environ.get("consumer_key"), os.environ.get("consumer_secret"))
+    auth.set_access_token(os.environ.get("access_token"), os.environ.get("access_token_secret"))
+    return tweepy.API(auth)
+
+
+def store_tweets(tweets):
+    data = {"id":[], "url":[], "text":[], ""}
+    df = pd.DataFrame()
+    for tweet in tweets:
+        pass
+
+
+
+def get_tweets(api):
+    f = api.search_tweets(q="covid", count=1)
+    pprint.pprint(f[0]._json)
+    pass
+
 
 def main():
-    auth = tp.OAuthHandler(consumer_key="qgVsLBKbmjFxDoodyWVX1k3dx", consumer_secret="8z3y934Na5IgfO4u791kDrzQ45EPDSmJfCGB6AvYIwoqM2Pu3w")
-    auth.set_access_token("1462918284601225216-E7VWlZvm2AJXOJAmg5HP94WdfNSjB4", "tvs1Phv1Bg4bKugrhDclZprbLvpaJBDHIo37NVBIl8hLc")
-
-    api = tweepy.API(auth)
-
-    # api.search_tweets()
-
-    f = api.search_tweets(q="covid", count=1)
-    print(len(f))
-
-    # print(pprint.pprint(f[0]._json))
+    api = authorize()
+    tweets = get_tweets(api)
 
 
 if __name__ == '__main__':
