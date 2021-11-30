99c90205248593b34f47e8ac8e8c31e826a19ec9
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2021-11-30 17:45:13 -0500

    ability to save csv

--- main2.py
+++ main2.py
@@ -1,11 +1,11 @@
 import pprint
-
 import pandas as pd
 import argparse
 import tweepy
 import tweepy as tp
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import os
@@ -21,23 +21,28 @@ def authorize():
     return tweepy.API(auth)
 
 
-def store_tweets(tweets):
-    data = {"id":[], "url":[], "text":[], ""}
-    df = pd.DataFrame()
+def store_tweets(tweets, output_file):
+    data = {"id": [], "url": [], "text": [], "topic": [], "emotion": []}
+    df = pd.DataFrame(data)
     for tweet in tweets:
-        pass
-
+        df = df.append({"id": tweet["id"],
+                        "url": tweet["entities"]["urls"]["url"],
+                        "text": tweet["text"],
+                        "topic": "",
+                        "emotion": ""}, ignore_index=True)
+    df.to_csv(output_file)
 
 
 def get_tweets(api):
     f = api.search_tweets(q="covid", count=1)
     pprint.pprint(f[0]._json)
-    pass
+    return f
 
 
 def main():
     api = authorize()
     tweets = get_tweets(api)
+    store_tweets(tweets, args.o)
 
 
 if __name__ == '__main__':
